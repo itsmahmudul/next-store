fea6cc335337f3389686c85cf9b3813aa684b4db
mahmudul11223344 <islamashraf128@gmail.com>
2025-08-22 23:15:01 +0600

    api

--- src/app/hooks/axios.js
+++ src/app/hooks/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance
 const api = axios.create({
-    baseURL: "https://next-store-backend-beryl.vercel.app/", // base URL for all requests
+    baseURL: "https://next-store-backend-beryl.vercel.app/api", // base URL for all requests
     headers: {
         "Content-Type": "application/json",
     },
